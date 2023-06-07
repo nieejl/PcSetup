d77298d9770921e52ff7205223f1d6b75739a275
Niels Ejlersen <NIEJ@bitcoinsuisse.com>
2023-06-07 10:17:39 +0200

    lol

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,5 @@
-oh-my-posh init pwsh --config "~/downloads/avit.omp.json" | Invoke-Expression
-Import-Module posh-git
 Import-Module -Name Terminal-Icons
-Import-Module PSReadLine
+Function gitlatestbranch {git for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'}
+Set-Alias -Name glb -Value gitlatestbranch
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -Colors @{ InlinePrediction = '#2F7004'}
