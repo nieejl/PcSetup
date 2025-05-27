7ad42320bc49bd6da0987468f745210ec936965f
FlipFlop <nille-ej@hotmail.com>
2025-05-27 16:37:00 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,10 @@
-Import-Module -Name Terminal-Icons
-Function gitlatestbranch {git for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'}
-Set-Alias -Name glb -Value gitlatestbranch
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -Colors @{ InlinePrediction = '#2F7004'}
+
+function gs { git status }
+function glog { git log --oneline }
+function gcom { git commit -m "wip" }
+function gpush { git push }
+
+Function gitlatestbranch {git for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'}
+Set-Alias -Name glb -Value gitlatestbranch
